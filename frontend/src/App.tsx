c26833a3ab244d202c4df3a86b0f4c38b1e90452
fiusks <rafabarros1@hotmail.com>
2022-04-19 21:17:30 -0300

    routes implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import './App.scss';
+import Router from './router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     Iai
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }
