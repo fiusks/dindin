9188495caae168b1f0796add5cd1678171bd5041
fiusks <rafabarros1@hotmail.com>
2022-05-06 08:09:52 -0300

    remove of detailed Transaction route

--- backend/src/router/routes.ts
+++ backend/src/router/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 const routes = Router()
-import {listAllTransactions,createTransaction,deleteTransaction,updateTransaction,detailTransaction} from '../controllers/TransactionsController';
+import {listAllTransactions,createTransaction,deleteTransaction,updateTransaction,listFilteredTransactions} from '../controllers/TransactionsController';
 
 
 routes.get('/transactions', listAllTransactions);
 routes.post('/transactions', createTransaction);
 routes.delete('/transactions/:id', deleteTransaction);
 routes.put('/transactions/:id', updateTransaction);
-routes.get('/transactions/:id', detailTransaction);
+routes.post('/filter',listFilteredTransactions)
 
 
 
