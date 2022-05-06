b163eaa8d4b94d3197760e764ad9199f9cfa7d57
fiusks <rafabarros1@hotmail.com>
2022-05-06 08:10:22 -0300

    Unecessary types for the transaction model

--- backend/src/models/transactions.ts
+++ backend/src/models/transactions.ts
@@ -1,30 +1,15 @@
-import {Schema,model} from "mongoose";
+import {Request} from 'express'
 
-interface TransactionDocument {
+export interface TransactionDocument {
+  id?:number,
   date:Date,
-  week_day:string,
+  weekDay?:string,
   description:string,
-  value:number,
+  amount:number,
   category:string,
   type:"credit"|"debit",
-  transactionID:number
 }
 
-const transactionSchema = new Schema<TransactionDocument>(
-    {
-        date:{type:Date,required:true},
-        week_day:{type:String,required:true},
-        description:{type:String,required:true},
-        value:{type:Number,required:true},
-        category:{type:String,required:true},
-        type:{type:String,enum:["credit","debit"],required:true},
-        transactionID:{type:Number,required:true}
-    },
-    {timestamps:true}
-)
-
-transactionSchema.index({transactionID:1},{unique:true})
-
-const Transaction = model<TransactionDocument>('Transaction',transactionSchema)
-
-export default Transaction
+export interface RequestTransaction extends Request{
+  body:TransactionDocument 
+}
