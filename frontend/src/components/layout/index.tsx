8fa6aabc4a95a87d50bcf95788abf5ea272c5980
fiusks <rafabarros1@hotmail.com>
2022-05-07 16:19:27 -0300

    implementation of filter and transaction context to layout

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,24 +1,30 @@
-import './style.scss';
+import './styles.scss';
 import { Outlet } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import logo from '../../assets/images/logo.svg';
+import TransactionProvider from '../../contexts/transactionContext';
+import FilterProvider from '../../contexts/filterContext';
 
 export default function Layout() {
   return (
-    <Container fluid className="p-0 layout-container">
-      <Row className="layout-header">
-        <Col>
-          <div className="header-container">
-            <img src={logo} alt="App logo" />
-            <h1>Dindin</h1>
-          </div>
-        </Col>
-      </Row>
-      <Row className="layout-body">
-        <Col>
-          <Outlet />
-        </Col>
-      </Row>
-    </Container>
+    <FilterProvider>
+      <TransactionProvider>
+        <Container fluid className="p-0 layout-container">
+          <Row className="layout-header">
+            <Col>
+              <div className="header-container">
+                <img src={logo} alt="App logo" />
+                <h1>Dindin</h1>
+              </div>
+            </Col>
+          </Row>
+          <Row className="layout-body">
+            <Col>
+              <Outlet />
+            </Col>
+          </Row>
+        </Container>
+      </TransactionProvider>
+    </FilterProvider>
   );
 }
