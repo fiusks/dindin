cc8b7ca6966d3777dd4fd5968f9b67b6c8d1bc5b
fiusks <rafabarros1@hotmail.com>
2022-05-07 16:15:55 -0300

    Filtered transactions implementation

--- backend/src/router/routes.ts
+++ backend/src/router/routes.ts
@@ -8,7 +8,7 @@ routes.get('/transactions', listAllTransactions);
 routes.post('/transactions', createTransaction);
 routes.delete('/transactions/:id', deleteTransaction);
 routes.put('/transactions/:id', updateTransaction);
-routes.post('/filter',listFilteredTransactions)
+routes.post('/filtertransactions',listFilteredTransactions)
 
 
 
