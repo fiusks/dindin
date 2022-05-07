22dd06e484c1b05bca8508905935b2ffeba2622b
fiusks <rafabarros1@hotmail.com>
2022-05-07 16:14:53 -0300

    transaction types updated model

--- backend/src/models/transactions.ts
+++ backend/src/models/transactions.ts
@@ -2,13 +2,14 @@ import {Request} from 'express'
 
 export interface TransactionDocument {
   id?:number,
-  date:Date,
-  weekDay?:string,
+  date:Date|string,
+  weekday?:string,
   description:string,
   amount:number,
   category:string,
   type:"credit"|"debit",
 }
+
 
 export interface RequestTransaction extends Request{
   body:TransactionDocument 
