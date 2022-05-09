a34c349dfff5f78a8165bf2e9332233ab926970d
fiusks <rafabarros1@hotmail.com>
2022-05-09 19:22:21 -0300

    process.env types correction

--- backend/enviroment.d.ts
+++ backend/enviroment.d.ts
@@ -1,10 +1,8 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
       PORT: string;
-      HOST: string;
-      DB_URI?: string;
       DB_HOST:string;
-      LALA_USER:string;
+      DB_USER:string;
       DB_PASSWORD:string;
       DB_DATABASE:string;
     }
