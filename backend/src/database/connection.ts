413fd2dcc5293f32c4b39c41dfa811ef9a61d536
fiusks <rafabarros1@hotmail.com>
2022-05-09 18:42:03 -0300

    Heroku db paramaters

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -3,14 +3,17 @@ dotenv.config()
 
 const knex = require("knex")({
     client: "pg",
-    version: "^8.7.1",
+    version: "^8.7.3",
     connection: {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST||"localhost",
       port: 5432,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER || process.env.DB_LOCAL_USER,
+      password: process.env.DB_PASSWORD||process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_DATABASE || process.env.DB_LOCAL_DATABASE,
     },
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
   
   export default knex
